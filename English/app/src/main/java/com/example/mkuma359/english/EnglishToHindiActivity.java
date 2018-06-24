d8b9a2c396ed22f0c7ecf93952d3dd622aa3c637
Anupam Yas Vardhan <anupam@Karankcs-MacBook-Pro.local>
2018-06-24 19:37:54 +0530

    Small changes

--- English/app/src/main/java/com/example/mkuma359/english/EnglishToHindiActivity.java
+++ English/app/src/main/java/com/example/mkuma359/english/EnglishToHindiActivity.java
@@ -115,7 +115,7 @@ class SearchPageRunnable implements Runnable {
             }
 
 
-            inputWord.setText(wordToSearch);
+            inputWord.setText(wordToSearch+"s");
             usage1.setText(commonUsageInHindiString1);
             usage2.setText(commonUsageInHindiString2);
             meaningInHindiMeaning.setText(meaning);
