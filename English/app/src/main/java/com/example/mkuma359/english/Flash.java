24a365fe1195a9dd23c4c6ad567562197dfc88bd
Anupam Yas Vardhan <anupam@Karankcs-MacBook-Pro.local>
2018-06-30 12:20:40 +0530

    Made changes to chat tab

--- English/app/src/main/java/com/example/mkuma359/english/Flash.java
+++ English/app/src/main/java/com/example/mkuma359/english/Flash.java
@@ -6,13 +6,9 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
-// Navigation
-
-public class MainActivity extends AppCompatActivity {
+public class Flash extends AppCompatActivity {
 
     private TextView mTextMessage;
 
@@ -23,9 +19,6 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-
-
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     startActivity(new Intent(getApplicationContext(),MainActivity.class));
@@ -41,29 +34,14 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-
-    // Navigation
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_flash);
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
-    // Search Result
-
-    public void sendMessage(View view)
-    {
-        EditText wordText = findViewById(R.id.edtType);
-        String wordString = wordText.getText().toString();
-
-        Intent intent = new Intent(this, SearchPageActivity.class);
-        intent.putExtra("inputWord", wordString);
-
-        startActivity(intent);
-    }
 }
